a7d11f7b8dd2d770f9816ab10cb24291a404b681
Adrian Palacios <ukonat@gmail.com>
2018-10-15 11:20:44 +0200

    Refine prop funs filtering

--- src/symgen.erl
+++ src/symgen.erl
@@ -6,6 +6,14 @@ main(File) ->
   FiltForms = forms:filter(fun is_prop_fun/1, Forms),
   io:format("~p~n", [FiltForms]).
 
-% TODO: Fix predicate for filtering proper calls
-is_prop_fun({function,_,_,0,_}) -> true;
+is_prop_fun({function,_,_,0,Clauses}) ->
+  % TODO: Change hd to something else?
+  case hd(Clauses) of
+    {clause,_,_,_,Body} ->
+      case hd(Body) of
+        {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},_} -> true;
+        _ -> false
+      end;
+    _ -> false
+  end;
 is_prop_fun(_) -> false.
