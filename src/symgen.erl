b9e4b270a810df27ba2cbb5cb457cbea27b8d693
Adrian Palacios <ukonat@gmail.com>
2018-10-15 17:55:11 +0200

    Traverse and print atts in prop funs

--- src/symgen.erl
+++ src/symgen.erl
@@ -3,17 +3,33 @@
 
 main(File) ->
   Forms = forms:read(File ++ ".erl"),
-  FiltForms = forms:filter(fun is_prop_fun/1, Forms),
-  io:format("~p~n", [FiltForms]).
+  PropFuns = forms:filter(fun is_prop_fun/1, Forms),
+  [generate_clauses(Fun) || Fun <- PropFuns].
 
-is_prop_fun({function,_,_,0,Clauses}) ->
-  % TODO: Change hd to something else?
-  case hd(Clauses) of
-    {clause,_,_,_,Body} ->
-      case hd(Body) of
-        {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},_} -> true;
-        _ -> false
-      end;
+
+is_prop_fun({function,_,_,0,FunClauses}) ->
+  FirstClause = hd(FunClauses),
+  Body = erl_syntax:clause_body(FirstClause),
+  case hd(Body) of
+    {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},_} -> true;
     _ -> false
   end;
 is_prop_fun(_) -> false.
+
+generate_clauses(Fun) ->
+  {function,_,_,0,Clauses} = Fun,
+  {clause,_,_,_,Body} = hd(Clauses),
+  {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},CallArgs} = hd(Body),
+  Types = lists:nth(1, CallArgs),
+  Prop  = lists:nth(2, CallArgs),
+  Vars = get_vars(Prop),
+  io:format("VARS:  ~s~n", [forms:from_abstract(Vars)]),
+  io:format("TYPES: ~s~n", [forms:from_abstract(Types)]),
+  io:format("PROP:  ~s~n", [forms:from_abstract(Prop)]).
+
+get_vars({'fun',_,{clauses,Clauses}}) ->
+  FirstClause = hd(Clauses),
+  {clause,_,Vars,_,_} = FirstClause,
+  Vars.
+  
+
