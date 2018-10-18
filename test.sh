ac9f2d8aa3cb6360e299cab6577f36cd2dd620ac
Adrian Palacios <ukonat@gmail.com>
2018-10-18 17:42:08 +0200

    Add examples to the test script

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ cd tests
 test_path="../src/"
 cmd_init="erl -noshell -pa ../ebin -run symgen main "
 cmd_end=" -s init stop"
-for test in biggest_ok; do
+for test in biggest_ok delete_bug; do
      $cmd_init$test$cmd_end # > $test_path$test".pl"
 done
