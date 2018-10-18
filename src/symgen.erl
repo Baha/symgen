cc55463f2cb4c504ae934e764c82a866de0016e6
Adrian Palacios <ukonat@gmail.com>
2018-10-18 17:43:17 +0200

    Consider SUCHTHAT and LET  definitions

--- src/symgen.erl
+++ src/symgen.erl
@@ -11,6 +11,8 @@ is_prop_fun({function,_,_,0,FunClauses}) ->
   Body = erl_syntax:clause_body(FirstClause),
   case hd(Body) of
     {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},_} -> true;
+    {call,_,{remote,_,{atom,_,proper_types},{atom,_,add_constraint}},_} -> true;
+    {call,_,{remote,_,{atom,_,proper_types},{atom,_,bind}},_} -> true;
     _ -> false
   end;
 is_prop_fun(_) -> false.
@@ -18,13 +20,23 @@ is_prop_fun(_) -> false.
 generate_clauses(Fun) ->
   {function,_,_,0,Clauses} = Fun,
   {clause,_,_,_,Body} = hd(Clauses),
-  {call,_,{remote,_,{atom,_,proper},{atom,_,forall}},CallArgs} = hd(Body),
+  {call,_,{remote,_,{atom,_,ProperMod},{atom,_,ProperCall}},CallArgs} = hd(Body),
   Types = lists:nth(1, CallArgs),
   Prop  = lists:nth(2, CallArgs),
   Vars = get_vars(Prop),
-  io:format("VARS:  ~s~n", [forms:from_abstract(Vars)]),
-  io:format("TYPES: ~s~n", [forms:from_abstract(Types)]),
-  io:format("PROP:  ~s~n", [forms:from_abstract(Prop)]),
+  FunName = erl_syntax:function_name(Fun),
+  io:format("~s -- ", [forms:from_abstract(FunName)]),
+  case {ProperMod, ProperCall} of
+    {proper,forall} ->
+      io:format("FORALL (property)~n");
+    {proper_types, add_constraint} ->
+      io:format("SUCHTHAT (user-defined type)~n");
+    {proper_types, bind} ->
+      io:format("LET (user-defined type)~n")
+  end,
+  io:format("Vars:  ~s~n", [forms:from_abstract(Vars)]),
+  io:format("Types: ~s~n", [forms:from_abstract(Types)]),
+  % io:format("PROP:  ~s~n", [forms:from_abstract(Prop)]),
   io:format("~n").
 
 get_vars({'fun',_,{clauses,Clauses}}) ->
